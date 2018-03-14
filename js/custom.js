1334ced89175297ed530567f2e485bd5a63dfeeb
pestra77 <pestra77@mail.ru>
2018-03-14 19:30:54 +0300

    linked nav

--- js/custom.js
+++ js/custom.js
@@ -14,4 +14,14 @@ $(function(){
   $('.option__title').click(function(event) {
     $('.option__list').toggleClass('option__list_active');
   });
+
+  $(".menu__link").click(function (e) {
+    e.preventDefault();
+    var elementClick = $(this).attr("href");
+    console.log(elementClick)
+    var destination = $('#' + elementClick).offset().top;
+    $('html').animate({ scrollTop: destination }, 800);
+
+    return false;
+  });
 });
